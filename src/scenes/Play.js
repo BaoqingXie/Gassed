aeba48246c11ec6583035a2d1a1e49adbe7445c9
JunYao <jli540@ucsc.edu>
2020-05-01 01:51:49 -0700

    Update Play.js

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,20 +5,31 @@ class Play extends Phaser.Scene {
 
     preload() {
         this.load.path = './assets/';
+
+
         this.load.image('floor','floor.png');
-        // bgcolor line should be replaced with loading bg image
-        // this.cameras.main.setBackgroundColor('#FACADE') // just so i can see the character
-        
-        //this.load.image('player1','guy.png');
+
         this.load.atlas('player1', 'player1.png', 'player1.json');
+
+        this.load.image('floor', 'floor.png');
+        this.load.image('fuelbar', 'fuelbar.png');
+        this.load.image('Burrito', 'Burrito.png');
+        this.load.image('Banana', 'Banana.png');
+
+        // can replace with bg asset
+        this.cameras.main.setBackgroundColor('#FACADE') // just so i can see the character
+
     }
 
+    create() {
+
+        //creating anims using the atlas - can tinker with
     create(){
         // place background tile sprite
         this.background = this.add.tileSprite(0, 0, 640, 480, 'background').setScale(1.25,1.25).setOrigin(0, 0);
         //creating anims using the atlas
         this.anims.create({
-            key: 'run',
+            key: 'run', // default running animation
             frames: this.anims.generateFrameNames('player1', {
                 start: 0,
                 end: 4,
@@ -28,11 +39,35 @@ class Play extends Phaser.Scene {
             frameRate: 15,
             repeat: -1
         });
-        // cycles through the entire fart animation (tap spacebar?)
+
         this.anims.create({
-            key: 'full-fart',
+            key: 'jump-fart', // plays entire fart animation
             frames: this.anims.generateFrameNames('player1', {
                 start: 0,
+                end: 9,
+                zeroPad: 2,
+                prefix: 'fart',
+            }),
+            frameRate: 12,
+            repeat: 0
+        });
+
+        this.anims.create({
+            key: 'loop-fart', // looping 2 frames while in the air (while holding spacebar?)
+            frames: this.anims.generateFrameNames('player1', {
+                start: 3,
+                end: 7,
+                zeroPad: 2,
+                prefix: 'fart',
+            }),
+            frameRate: 8,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'falling', // fart dissipating (tap spacebar?)
+            frames: this.anims.generateFrameNames('player1', {
+                start: 6,
                 end: 9,
                 zeroPad: 2,
                 prefix: 'fart',
@@ -40,21 +75,9 @@ class Play extends Phaser.Scene {
             frameRate: 8,
             repeat: 0
         });
-        // looping 2 frames while in the air (while holding spacebar?)
+
         this.anims.create({
-            key: 'fart',
-            frames: this.anims.generateFrameNames('player1', {
-                start: 4,
-                end: 5,
-                zeroPad: 2,
-                prefix: 'fart',
-            }),
-            frameRate: 8,
-            repeat: -1
-        });
-        // death animation
-        this.anims.create({
-            key: 'death',
+            key: 'death', // death animation
             frames: this.anims.generateFrameNames('player1', {
                 start: 0,
                 end: 7,
@@ -65,36 +88,54 @@ class Play extends Phaser.Scene {
             repeat: 0
         });
 
+
+        //timer for the game score
+        timerEvent = this.time.addEvent({
+            delay: 600,                // ms
+            callback: this.printTime,
+            //args: [],
+            callbackScope: this,
+            loop: true
+        });
+        this.elapsed = timerEvent.getRepeatCount();
+        this.time = 0;
+
+        //text
+        this.text = this.add.text(690, 20);
+
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
-        this.player1 = this.physics.add.sprite(centerX,centerY,'player1');
-        this.player1.play('run'); // default run animation
+        this.player1 = this.physics.add.sprite(centerX, centerY, 'player1').setScale(0.5);
+        this.player1.setFrame('death00'); // this is the idle frame
         this.player1.setGravityY(500);
+        this.player1.setCollideWorldBounds(true);
 
-        this.floor = this.physics.add.sprite(centerX,this.game.config.height*0.90,'floor');
-        this.floor.displayWidth = this.sys.game.config.width*1.1;
-        this.floor.displayHeight = this.game.config.height*0.05
+        this.floor = this.physics.add.sprite(centerX, this.game.config.height * 0.90, 'floor');
+        this.floor.displayWidth = this.sys.game.config.width * 1.1;
+        this.floor.displayHeight = this.game.config.height * 0.05
         this.floor.setImmovable();
 
-        this.physics.add.collider(this.player1,this.floor);
+        this.physics.add.collider(this.player1, this.floor);
+
+        //health bar
+        this.hp = new HealthBar(this, 20, 20);
+
+        //add items
+        this.banana = this.physics.add.sprite(centerX, centerY - 200, 'Banana');
+        this.banana.body.setSize(10, 10)
+        this.banana.body.setImmovable(true);
 
 
+        this.burrito = this.physics.add.sprite(centerX, centerY - 100, 'Burrito');
+        this.burrito.body.setSize(10, 10)
+        this.burrito.body.setImmovable(true);
 
-<<<<<<< Updated upstream
-        this.grounded = false;
-        this.justJumped = false;
-=======
 
->>>>>>> Stashed changes
 
     }
 
     update() {
 
-<<<<<<< Updated upstream
-        
-        this.background.tilePositionX += 4;  // scroll tile sprite
-=======
 
         if (this.player1.y + this.player1.height >= this.floor.y - 1 && this.grounded == false) {
 <<<<<<< HEAD
@@ -109,29 +150,58 @@ class Play extends Phaser.Scene {
             if (!this.justJumped)
                 this.player1.play('run'); // when grounded/not jumping, play run animation
         }
->>>>>>> Stashed changes
 
-        if (this.player1.y + this.player1.height >= this.floor.y - 1 && this.grounded == false)
-            this.grounded = true;
-            
         if (this.player1.y + this.player1.height < this.floor.y - 1 && this.grounded == true)
             this.grounded = false;
 
-        if (Phaser.Input.Keyboard.JustDown(keySPACE) && this.grounded){
+        if (Phaser.Input.Keyboard.JustDown(keySPACE) && this.grounded && this.hp.value > 0) {
+
             this.justJumped = true;
+            this.player1.anims.stop();
+            this.player1.setFrame('fart09');
             console.log('jump');
-            this.player1.setVelocity(0,-400);
+            this.player1.setVelocity(0, -400);
             this.grounded = false;
-            setTimeout(() => {this.justJumped = false;}, 500);
+            setTimeout(() => { this.justJumped = false; }, 500);
+            this.hp.decrease(10);
         }
 
-        if (keySPACE.isDown && !this.grounded && !this.justJumped){
+        if (keySPACE.isDown && !this.grounded && !this.justJumped && this.hp.value > 0) {
             console.log('fart');
-            this.player1.setAcceleration(0,-950);
+            if (!this.isFarting) {
+                this.player1.play('loop-fart');
+                this.isFarting = true;
+            }
+            this.hp.decrease(0.1);
+            this.player1.setAcceleration(0, -950);
         }
         else {
-            this.player1.setAcceleration(0,0);
+            this.player1.setAcceleration(0, 0);
+            if (this.isFarting && !this.grounded) {
+                this.player1.play('falling');
+                this.isFarting = false;
+            }
         }
 
+        this.physics.overlap(this.player1, this.burrito, this.increase, null, this);
+        this.physics.overlap(this.player1, this.banana, this.decrease, null, this);
+
+    }
+    increase() {
+        this.burrito.destroy(true);
+        this.hp.increase(20);
+        console.log('increase');
+    }
+
+    decrease() {
+        this.banana.destroy(true);
+        this.hp.decrease(20);
+        console.log('decrease');
+    }
+
+    printTime() {
+        console.log(this.time);
+        this.text.setText('Score: ' + this.time);
+        this.time += 1;
     }
 }
