98a24a3796c84fb03a5457028c4c70238d5e5c23
BaoqingXie <bxie4@ucsc.edu>
2020-05-03 22:36:26 -0700

    credit texts and main explain

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -35,11 +35,16 @@ class Credits extends Phaser.Scene {
         let textSpacer = 60;
 
 
-        this.add.text(centerX- 3.8 * textSpacer, centerY - 3* textSpacer, 'Presented By The Cooler Team', titleConfig).setOrigin(0, 0);
-        this.add.text(centerX - 3.8 * textSpacer, centerY - 2 * textSpacer, 'Alec Zhang', smallConfig).setOrigin(0, 0);
-        this.add.text(centerX - 3.8 * textSpacer, centerY - 1 * textSpacer, 'Brad Kim', smallConfig).setOrigin(0, 0);
-        this.add.text(centerX - 3.8 * textSpacer, centerY, 'Baoqing Xie', smallConfig).setOrigin(0, 0);
-        this.add.text(centerX - 3.8 * textSpacer, centerY + 1 * textSpacer, 'Junyao Li', smallConfig).setOrigin(0, 0);
+        this.add.text(centerX - 3.8 * textSpacer, centerY - 3.5 * textSpacer, 'Presented By The Cooler Team', titleConfig).setOrigin(0, 0);
+        this.add.text(centerX - 3.8 * textSpacer, centerY - 2.5 * textSpacer, 'Alec Zhang', smallConfig).setOrigin(0, 0);
+        this.add.text(centerX - 3.8 * textSpacer, centerY - 2 * textSpacer, 'Brad Kim', smallConfig).setOrigin(0, 0);
+        this.add.text(centerX - 3.8 * textSpacer, centerY - 1.5 * textSpacer, 'Baoqing Xie', smallConfig).setOrigin(0, 0);
+        this.add.text(centerX - 3.8 * textSpacer, centerY - 1 * textSpacer, 'Junyao Li', smallConfig).setOrigin(0, 0);
+        this.add.text(centerX - 3.8 * textSpacer, centerY, 'Outside Resource:', smallConfig).setOrigin(0, 0);
+        this.add.text(centerX - 3.8 * textSpacer, centerY + 1 * textSpacer, 'logo: Textcraft.com', smallConfig).setOrigin(0, 0);
+        this.add.text(centerX - 3.8 * textSpacer, centerY + 1.5 * textSpacer, 'gasbar.js: https://phaser.io/examples', smallConfig).setOrigin(0, 0);
+        this.add.text(centerX - 3.8 * textSpacer, centerY + 2 * textSpacer, 'SoundEffects: https://freesound.org', smallConfig).setOrigin(0, 0);
+        this.add.text(centerX - 3.8 * textSpacer, centerY + 2.5 * textSpacer, 'IdeasaboutItemsAssets: https://opengameart.org/', smallConfig).setOrigin(0, 0);
 
         this.Back = this.add.sprite(100, 50, 'Back', 1).setScale(0.5, 0.5);
         this.Back.setInteractive({
@@ -55,7 +60,7 @@ class Credits extends Phaser.Scene {
         });
 
         this.input.on('gameobjectdown', (pointer, gameObject, event) => {
-            this.sound.play('Selection', {volume:0.25});
+            this.sound.play('Selection', { volume: 0.25 });
             this.scene.start("menuScene");
         });
 
